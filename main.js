a12b1a5d3aafed81d6f1a5006dc5379d9de4d6b9
WillBarouch <93062709+WillBarouch@users.noreply.github.com>
2022-04-26 10:28:40 +1000

    0.1.1

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        autoHideMenuBar: true,
+        titleBarStyle: 'hidden',
+        titleBarOverlay: {
+            color: '#2e3440',
+            symbolColor: '#d8dee9'
+        },
+        icon: 'favicon/favicon.ico'
+
     })
 
     win.loadFile('index.html')
@@ -11,8 +19,9 @@ app.whenReady().then(() => {
     createWindow()
 })
 
+app.on = function (windowAllClosed, param2) {
+
+}
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
-
-``
