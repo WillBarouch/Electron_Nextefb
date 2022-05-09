a005fd8af11510a5b19691fb832f7d3a708f86f2
WillBarouch <93062709+WillBarouch@users.noreply.github.com>
2022-05-09 19:00:00 +1000

    0.3
    Preparing for 0.3.x release

---
+++ electron_nextefb-win32-x64/resources/app/main.js
@@ -0,0 +1,28 @@
+const {app, BrowserWindow} = require('electron')
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        autoHideMenuBar: true,
+        titleBarStyle: 'hidden',
+        frame: false,
+        "titleBarOverlay": {
+            color: '#2e3440',
+            symbolColor: '#d8dee9'
+        },
+        icon: 'favicon/favicon.ico'
+
+    })
+
+    win.loadFile('index.html')
+}
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on = function (windowAllClosed, param2) {
+
+}
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
